7359314765dff18e079b750baf31284f7c207881
Martin Poepel <martin.poepel@5minds.de>
2021-10-22 16:52:16 +0200

    Add features

---
+++ CSharp10Features.RecordTypesSealToString/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CSharp10Features.RecordTypesSealToString;
+
+public record Person(string FirstName, string LastName)
+{
+    public sealed override string ToString()
+    {
+        return $"Hi my name is: {FirstName} {LastName}";
+    }
+
+    public virtual string ToFullName()
+    {
+        return string.Empty;
+    }
+}
+
+public record Client(string FirstName, string LastName, string EMail) 
+    : Person(FirstName, LastName)
+{
+    public override string ToString()
+    {
+        return $"Hi my name is: {FirstName} {LastName}";
+    }
+
+    public sealed override string ToFullName()
+    {
+        return base.ToString();
+    }
+}
+
+public record Customer(string FirstName, string LastName, string EMail) : Client(FirstName, LastName, EMail)
+{
+    public override string ToFullName()
+    {
+        return $"Hello, I'am {FirstName} {LastName} and you can reach me through: {EMail}";
+    }
+}
